05d56f28cdefdfd44696c66ced89d99fc21f23bf
Dhruv Sheth <123584784+Dhruvnet@users.noreply.github.com>
2023-08-23 17:20:59 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="bg-indigo-600 py-6">
         <h1 className="text-white text-2xl font-semibold text-center font-serif">Product List
         <span className="text-gray-300 text-sm ml-2">-By Dhruv Sheth</span></h1>
-      </header
+      </header>
+
       <Data />
     </div>
   );
